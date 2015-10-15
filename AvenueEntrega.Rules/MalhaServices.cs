4d22db0ccfa40202a47eb1184b3157c564768d94
Willismar <cyberwillis@gmail.com>
2015-10-14 23:28:12 -0300

    Rules Project will process Domain Services from now on

--- AvenueEntrega.Rules/MalhaServices.cs
+++ AvenueEntrega.Rules/MalhaServices.cs
@@ -1,16 +1,20 @@
-﻿using AvenueEntrega.Model;
+﻿using AvenueEntrega.Model.Entities;
 
 namespace AvenueEntrega.Rules
 {
-    public static class MalhaServices
+    public class MalhaServices
     {
-        public static float EncontrarMelhorRota(this Mapa mapa, string nomeMapa, string origem, string destino, float autonomia)
+        public float EncontrarMelhorRota(Mapa mapa, string origem, string destino, float autonomia)
         {
-            //TODO: calculo da melhor roda por A*
-            //saber se origem esta no mapa
-            //saber se destino esta no mapa
-            //saber se existe um trajeto ate o destino
+            if (mapa != null)
+            {
+                var malha = new Malha(mapa.Rotas);
 
+                //TODO: calculo da melhor roda por A*
+                //saber se origem esta no mapa
+                //saber se destino esta no mapa
+                //saber se existe um trajeto ate o destino
+            }
             return 0.0f;
         }
     }
