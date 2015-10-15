e0ba9ea02e5d6409753a3129b7bed9c2bc54e09d
Willismar <cyberwillis@gmail.com>
2015-10-14 23:53:31 -0300

    few changes in unittest

--- AvenueEntrega.Model/Malha.cs
+++ AvenueEntrega.Model/Malha.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using AvenueEntrega.Model.Entities;
 
 namespace AvenueEntrega.Model
 {
--- AvenueEntrega.Services.UnitTest/Test_Model_Aggregation.cs
+++ AvenueEntrega.Services.UnitTest/Test_Model_Aggregation.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AvenueEntrega.Model;
+using AvenueEntrega.Model.Entities;
 using NUnit.Framework;
+using Malha = AvenueEntrega.Model.Malha;
 
 namespace AvenueEntrega.Services.UnitTest
 {
