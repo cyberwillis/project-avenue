3568cb7307ca38dadee613026677c65ce9b78218
Willismar <cyberwillis@gmail.com>
2015-10-14 23:47:36 -0300

    Changes in Service Layer, initial structure only

---
+++ AvenueEntrega.Services/MalhaServices.cs
@@ -0,0 +1,14 @@
+﻿using AvenueEntrega.Model.Repository;
+
+namespace AvenueEntrega.Services
+{
+    public class MalhaServices
+    {
+        private readonly IMapaRepository _mapaRepository;
+
+        public MalhaServices(IMapaRepository mapaRepository)
+        {
+            _mapaRepository = mapaRepository;
+        }
+    }
+}
