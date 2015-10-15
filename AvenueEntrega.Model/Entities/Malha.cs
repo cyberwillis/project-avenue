ca121f21c10148cde46cef3a3bc73e341c15621f
Willismar <cyberwillis@gmail.com>
2015-10-15 09:14:42 -0300

    changes on domain

--- AvenueEntrega.Model/Entities/Malha.cs
+++ AvenueEntrega.Model/Entities/Malha.cs
@@ -4,23 +4,44 @@ namespace AvenueEntrega.Model.Entities
 {
     public class Malha
     {
+        private IList<Rota> _rotas;
+        //public Dictionary<string, IList<Rota>> Direcoes { get; protected set; }
+
+
         public Malha(IList<Rota> rotas)
         {
-            var malha = new Dictionary<string, IList<Rota>>();
+            this._rotas = rotas;
 
-            foreach (var rota in rotas)
+            //Direcoes = new Dictionary<string, IList<Rota>>();
+
+            ////TODO: otimize , it exponential
+            //foreach (var rota in rotas)
+            //{
+            //    //source to destiny test
+            //    if (this.Direcoes.ContainsKey(rota.Origem))
+            //        this.Direcoes[rota.Origem].Add(rota); //add/update
+            //    else
+            //        this.Direcoes.Add(rota.Origem, new List<Rota>() { rota }); //create
+
+            //    //destination to source test
+            //    if (this.Direcoes.ContainsKey(rota.Destino))
+            //        this.Direcoes[rota.Destino].Add(rota); //add/update
+            //    else
+            //        this.Direcoes.Add(rota.Destino, new List<Rota>() { rota }); //create
+            //}
+        }
+
+        public IList<Rota> PossibleActions(string ponto)
+        {
+            var possibilidades = new List<Rota>();
+
+            foreach (var rota in _rotas)
             {
-                if (malha.ContainsKey(rota.Origem))
-                {
-                    //add/update
-                    malha[rota.Origem].Add(rota);
-                }
-                else
-                {
-                    //create
-                    malha.Add(rota.Origem, new List<Rota>() { rota });
-                }
+                if (rota.Origem.Equals(ponto) || rota.Destino.Equals(ponto))
+                    possibilidades.Add(rota);
             }
+
+            return possibilidades;
         }
     }
 }
