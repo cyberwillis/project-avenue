47eff316d512348b7abb154535ad54d1cf40553c
Willismar <cyberwillis@gmail.com>
2015-10-15 23:40:51 -0300

    Renamed DomainService class, created state entity only for localization purposes, and removed extension method class not needed

---
+++ AvenueEntrega.Rules/State.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace AvenueEntrega.Rules
+{
+    public class State
+    {
+        public string Name { get; }
+        public float Cost { get; }
+        public State Parent { get; }
+        public State(string name, State parent, float cost)
+        {
+            Name = name;
+            Parent = parent;
+            Cost = cost;
+        }
+
+        /// <summary>
+        /// This method trace back the path recursively by the parents
+        /// </summary>
+        /// <returns></returns>
+        public IList<string> GetPath()
+        {
+            var path = new List<string>();
+
+            if (Parent != null)
+                path.AddRange(Parent.GetPath());
+
+            path.Add(Name);
+
+            return path;
+        }
+
+        //public float GetTotalCost()
+        //{
+        //    float TotalCost = 0f;
+
+        //    if (Parent != null)
+        //        TotalCost += Parent.GetTotalCost();
+
+        //    TotalCost += Cost;
+
+        //    return TotalCost;
+        //}
+    }
+}
