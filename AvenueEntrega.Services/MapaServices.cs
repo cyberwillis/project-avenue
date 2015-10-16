28354aa1f7397443866d95c851f2c1f97ff6b64c
Willismar <cyberwillis@gmail.com>
2015-10-16 02:27:21 -0300

    changed .net version

---
+++ AvenueEntrega.Services/MapaServices.cs
@@ -0,0 +1,42 @@
+﻿using AvenueEntrega.DataContracts.Messages.Mapa;
+using AvenueEntrega.Model.Repository;
+using AvenueEntrega.ServiceContracts;
+
+namespace AvenueEntrega.Services
+{
+    public class MapaServices : IMapaService
+    {
+        private readonly IMapaRepository _mapaRepository;
+
+        public MapaServices(IMapaRepository mapaRepository)
+        {
+            _mapaRepository = mapaRepository;
+        }
+
+
+        public EncontrarTodosMapasResponse EncontrarTodosMapas()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ExcluirMapaResponse ExcluirMapa(ExcluirMapaRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AlterarMapaResponse AlterarMapa(AlterarMapaRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public InserirMapaResponse InserirMapa(InserirMapaRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public EncontrarMapaPorResponse EncontrarMapaPor(EncontrarMapaPorRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
