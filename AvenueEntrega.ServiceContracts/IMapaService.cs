62a618d62fadef2cf0b233eaeef8f5b63c0c3e1d
Willismar <cyberwillis@gmail.com>
2015-10-16 02:27:06 -0300

    created service contracts project

---
+++ AvenueEntrega.ServiceContracts/IMapaService.cs
@@ -0,0 +1,13 @@
+﻿using AvenueEntrega.DataContracts.Messages.Mapa;
+
+namespace AvenueEntrega.ServiceContracts
+{
+    public interface IMapaService
+    {
+        EncontrarTodosMapasResponse EncontrarTodosMapas();
+        EncontrarMapaPorResponse EncontrarMapaPor(EncontrarMapaPorRequest request);
+        InserirMapaResponse InserirMapa(InserirMapaRequest request);
+        AlterarMapaResponse AlterarMapa(AlterarMapaRequest request);
+        ExcluirMapaResponse ExcluirMapa(ExcluirMapaRequest request);
+    }
+}
