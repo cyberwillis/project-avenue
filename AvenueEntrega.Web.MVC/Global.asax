eec81a4114f1069fd7f130b2666bb6e86dc038dc
Willismar <cyberwillis@gmail.com>
2015-10-16 12:27:22 -0300

    Created UI project

---
+++ AvenueEntrega.Web.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AvenueEntrega.Web.MVC.MvcApplication" Language="C#" %>
---
+++ AvenueEntrega.Web.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AvenueEntrega.Web.MVC.Startup))]
+namespace AvenueEntrega.Web.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
