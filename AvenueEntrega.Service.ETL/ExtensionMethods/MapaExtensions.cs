c81b4753716cbbf4404c7454f2400841e29cbd98
Willismar <cyberwillis@gmail.com>
2015-10-16 19:30:49 -0300

    Service for ETL management

---
+++ AvenueEntrega.Service.ETL/ExtensionMethods/MapaExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AvenueEntrega.Model.Entities;
+using AvenueEntrega.RepositoryFile.Dto;
+
+namespace AvenueEntrega.Service.ETL.ExtensionMethods
+{
+    public static class MapaExtensions
+    {
+        public static MapaRepositoryDto ConvertoToMapaRepositoryDto( this Mapa mapa)
+        {
+            var mapaDto = new MapaRepositoryDto();
+            //mapaDto.Id = mapa.Id.ToString();
+            mapaDto.NomeMapa = mapa.NomeMapa;
+
+            if (mapa.Rotas.Count > 0)
+                mapaDto.Rotas = mapa.Rotas.ConvertToListRotasDto();
+
+            return mapaDto;
+        }
+
+        public static IList<MapaRepositoryDto> ConvertToListMapasDto(this IList<Mapa> mapas)
+        {
+            var mapasDto = mapas.Select(mapa => mapa.ConvertoToMapaRepositoryDto()).ToList();
+
+            return mapasDto;
+        }
+
+        public static Mapa ConvertToMapa(this MapaRepositoryDto mapaRepositoryDto)
+        {
+            var mapa = new Mapa();
+            //if(!string.IsNullOrEmpty(mapaDto.Id))
+            //    mapa.Id = Guid.Parse(mapaDto.Id);
+
+            mapa.NomeMapa = mapaRepositoryDto.NomeMapa;
+
+            if (mapaRepositoryDto.Rotas.Count > 0)
+                mapa.Rotas = mapaRepositoryDto.Rotas.ConvertToListRotas();
+
+            return mapa;
+        }
+
+        public static IList<Mapa> ConvertToListMapas(this IList<MapaRepositoryDto> mapasRepositoryDto)
+        {
+            var mapas = mapasRepositoryDto.Select(mapaDto => mapaDto.ConvertToMapa()).ToList();
+
+            return mapas;
+        }
+
+    }
+}
---
+++ AvenueEntrega.Service.ETL/FileServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using AvenueEntrega.RepositoryFile;
+using AvenueEntrega.RepositoryFile.Messages.Mapa;
+using AvenueEntrega.Service.ETL.ExtensionMethods;
+
+namespace AvenueEntrega.Service.ETL
+{
+    public class FileServices
+    {
+        private MapaFileRepository _mapaFileRepository;
+
+        public FileServices(MapaFileRepository mapaFileRepository)
+        {
+            _mapaFileRepository = mapaFileRepository;
+        }
+
+        public CarregarMapaResponse LoadMapaFromFile(CarregarMapaRequest request)
+        {
+            var response = new CarregarMapaResponse();
+            try
+            {
+                var result = _mapaFileRepository.FindBy(request.Arquivo, request.NomeMapa);
+                if (result != null)
+                {
+                    response.Mapa = result;
+                    response.Success = true;
+                    response.Message = "Arquivo processado com sucesso.";
+                }
+                else
+                {
+                    response.Success = false;
+                    response.Message = "Arquivo não encontrado.";
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = "Erro: " + ex.Message;
+            }
+            return response;
+        }
+
+        //public Mapa LoadMapaFromFile(string fileName, string name)
+        //{
+        //    var result = _mapaRepository.FindBy(fileName, name);
+
+        //    if (result != null)
+        //         return result.ConvertToMapa();
+
+        //    return null;
+        //}
+    }
+}
