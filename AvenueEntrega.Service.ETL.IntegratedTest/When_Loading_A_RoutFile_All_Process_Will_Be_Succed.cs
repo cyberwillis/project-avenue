cb2d7b93bbdd45630f5a8f0feff53666e423fcd0
Willismar <cyberwillis@gmail.com>
2015-10-16 19:28:26 -0300

    Loading of txt by ETL project and tests

---
+++ AvenueEntrega.Service.ETL.IntegratedTest/When_Loading_A_RoutFile_All_Process_Will_Be_Succed.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using AvenueEntrega.Model.Entities;
+using AvenueEntrega.RepositoryFile;
+using AvenueEntrega.RepositoryFile.Messages.Mapa;
+using AvenueEntrega.RepositoryNHibernate;
+using AvenueEntrega.RepositoryNHibernate.Repositories;
+using AvenueEntrega.Service.ETL.ExtensionMethods;
+using NUnit.Framework;
+
+namespace AvenueEntrega.Service.ETL.IntegratedTest
+{
+    [TestFixture]
+    public class When_Loading_A_RoutFile_All_Process_Will_Be_Succed
+    {
+        private FileServices _fileServices;
+        private string _fileName;
+
+        [SetUp]
+        public void Init()
+        {
+            this._fileName = AppDomain.CurrentDomain.BaseDirectory + "\\TestMapa.txt";
+
+            //string codeBase = Assembly.GetExecutingAssembly().FullName;
+            //UriBuilder uri = new UriBuilder(codeBase);
+            //string filePath = Uri.UnescapeDataString(uri.Path);
+            //this._fileName = codeBase; //filePath += "\\" + this._fileName;
+            //Path.GetDirectoryName(path);
+
+            var fileRepository = new MapaFileRepository();
+            var mapaRepository = new MapaRepository(new NHUnitOfWork<Mapa>(new NHUnitOfWorkRepository<Mapa>()));
+
+            this._fileServices = new FileServices(fileRepository);
+        }
+
+        [Test]
+        public void Test1_Loading_ClientExampleFile_Will_Pass()
+        {
+            var nomeMapa = "MyMap";
+            
+            var request = new CarregarMapaRequest()
+            {
+                NomeMapa = nomeMapa,
+                Arquivo = this._fileName
+            };
+
+            var response = this._fileServices.LoadMapaFromFile(request);
+
+            var mapa = response.Mapa.ConvertToMapa();
+
+            foreach (var rota in mapa.Rotas)
+            {
+                Console.WriteLine("{0} -> {1} = {2}", rota.Origem, rota.Destino, rota.Custo);
+            }
+
+            Assert.IsTrue(mapa.NomeMapa.Equals(nomeMapa));
+            Assert.IsTrue(mapa.Rotas.Count == 6);
+        }
+    }
+}
---
+++ AvenueEntrega.Service.ETL/ExtensionMethods/RotaExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AvenueEntrega.Model.Entities;
+using AvenueEntrega.RepositoryFile.Dto;
+
+namespace AvenueEntrega.Service.ETL.ExtensionMethods
+{
+    public static class RotaExtensions
+    {
+        public static RotaRepositoryDto ConvertToRotaDto( this Rota rota)
+        {
+            var rotaDto = new RotaRepositoryDto();
+            //rotaDto.Id = rota.Id.ToString();
+            rotaDto.Origem = rota.Origem;
+            rotaDto.Destino = rota.Destino;
+            rotaDto.Custo = rota.Custo;
+
+            return rotaDto;
+        }
+
+        public static IList<RotaRepositoryDto> ConvertToListRotasDto( this IList<Rota> rotas)
+        {
+            var rotasDto = rotas.Select(rota => rota.ConvertToRotaDto()).ToList();
+
+            return rotasDto;
+        }
+
+
+        public static Rota ConvertToRota(this RotaRepositoryDto rotaDto)
+        {
+            var rota = new Rota();
+            //if(!string.IsNullOrEmpty(rotaDto.Id))
+
+            rota.Id = Guid.NewGuid();
+            rota.Origem = rotaDto.Origem;
+            rota.Destino = rotaDto.Destino;
+            rota.Custo = rotaDto.Custo;
+
+            return rota;
+        }
+
+        public static IList<Rota> ConvertToListRotas(this IList<RotaRepositoryDto> rotasDto)
+        {
+            var rotas = rotasDto.Select(rotaDto => rotaDto.ConvertToRota()).ToList();
+
+            return rotas;
+        }
+    }
+}
