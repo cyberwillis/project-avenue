8f9a2b872e1505168f15dc9f278fa74fc838b4b9
Willismar <cyberwillis@gmail.com>
2015-10-18 21:57:49 -0200

    removed MongoDB shell structure and EntityFramework

--- AvenueEntrega.RepositoryEntityFramework/Migrations/MapaModelConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace AvenueEntrega.RepositoryEntityFramework.Migrations
-{
-    public class MapaModelConfiguration : CreateDatabaseIfNotExists<MapaContext>
-    {
-         
-    }
-}
